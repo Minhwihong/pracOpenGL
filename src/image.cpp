54866bf828b5a9aaaab7acc257bedc4bb1f2ca10
minhwihong <hidellok@gmail.com>
2022-04-24 22:19:30 +0900

    image checker function implement

--- src/image.cpp
+++ src/image.cpp
@@ -21,6 +21,40 @@ Image::~Image(){
     }
 }
 
+
+std::unique_ptr<Image> Image::Create(int width, int height, int channelCount = 4){
+    // parameter로 넘어온 세팅을 이용하여 거기에 맞는 empty 메모리를 확보하고 포인터 반납 
+
+    auto image = std::unique_ptr<Image>(new Image());
+
+    if(!image->Allocate(width, height, channelCount)){
+        return nullptr;
+    }
+
+    return std::move(image);
+}
+
+
+void Image::SetCheckImage(int gridX, int gridY){
+    // 
+
+}
+
+
+
+bool Image::Allocate(int width, int height, int channelCount){
+    m_width = width;
+    m_height = height;
+
+    m_channelCount = channelCount;
+
+    m_data = (uint8_t*)malloc(m_width*m_height*m_channelCount);
+
+    return m_data != nullptr ? true : false;
+
+}
+
+
 bool Image::LoadWithStb(const std::string& filepath){
 
     m_data = stbi_load(filepath.c_str(), &m_width, &m_height, &m_channelCount, 0);

a0516c1ac37e644745c6777c17317d57abbf836d
minhwihong <hidellok@gmail.com>
2022-06-07 22:38:18 +0900

    specular tested

--- shader/lighting.fs
+++ shader/lighting.fs
@@ -9,12 +9,23 @@ uniform vec3 lightPos;
 uniform vec3 lightColor;        // 광원의 색상
 uniform vec3 objectColor;       
 uniform float ambientStrength;  // 주변광(물체의 기본 색상)의 전체 밝기를 나타냄
+
+uniform float specularStrength;
+uniform float specularShininess;
+uniform vec3 viewPos;           // 보고 있는 눈(카메라)의 위치
+
+
 // uniform sampler2D tex;
 // uniform sampler2D tex2;
 
-void main() {
-    vec3 ambient = ambientStrength * lightColor;
 
+void main() {
+    /* ****************** 주변광(기본 광원) 계산 ****************** */
+    vec3 ambient = ambientStrength * lightColor;
+    /* ******************************************************* */
+
+
+    /* ************** 광원이 물체에 반사되는 광량 계산 *************** */
     vec3 lightDir = normalize(lightPos - position);
 
     // openGL program -> vertex shader -> 에서 넘어온 normal을 다시 normalize 하는 이유
@@ -22,11 +33,23 @@ void main() {
     // 단위벡터간의 선형보간 결과가 단위벡터임을 보장하지 못하기때문에 다시 normalize과정을 fragment shader과정에 진행
     vec3 pixelNorm = normalize(normal);
 
-
     vec3 diffuse = max(dot(pixelNorm, lightDir), 0.0) * lightColor; // ignore minus val of dot product
+    /* ******************************************************* */
 
 
-    vec3 result = (ambient + diffuse) * objectColor;
+    /* ********* 광원이 물체에 반사되어 카메라에 비치는 광량 계산 ******** */
+    vec3 viewDir = normalize(viewPos - position);
+
+    vec3 reflectDir = reflect(-lightDir, pixelNorm);
+
+    // 물체에 반사광이 표시되는 영역의 크기를 조절 -> specularShininess
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), specularShininess );
+
+    vec3 specular = specularStrength * spec * lightColor;
+    /* ******************************************************* */
+
+
+    vec3 result = (ambient + diffuse + specular) * objectColor;
 
     fragColor = vec4(result, 1.0);
     //fragColor = texture(tex, texCoord)*0.8 + texture(tex2, texCoord) * 0.2;
--- src/context.cpp
+++ src/context.cpp
@@ -54,6 +54,9 @@ void Context::Render() {
             ImGui::ColorEdit3("light color", glm::value_ptr(m_lightColor));
             ImGui::ColorEdit3("object color", glm::value_ptr(m_objectColor));
             ImGui::SliderFloat("ambient strength", &m_ambientStrength, 0.0f, 1.0f);
+
+            ImGui::SliderFloat("specular strength", &m_specularStrength, 0.0f, 1.0f);
+            ImGui::DragFloat("specular shininess", &m_specularShininess, 1.0f, 1.0f, 256.0f);
         }
 
         ImGui::Checkbox("animation", &m_animation);
@@ -101,6 +104,10 @@ void Context::Render() {
     m_program->SetUniform("lightColor", m_lightColor);
     m_program->SetUniform("objectColor", m_objectColor);
     m_program->SetUniform("ambientStrength", m_ambientStrength);
+
+    m_program->SetUniform("viewPos", m_cameraPos);
+    m_program->SetUniform("specularStrength", m_specularStrength);
+    m_program->SetUniform("specularShininess", m_specularShininess);
 
     for(int idx=0; idx<cubePositions.size(); ++idx){
         auto& pos = cubePositions[idx];

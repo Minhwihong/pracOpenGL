4973f0dbd4b63c8438210c6a9b6476aac839503d
minhwihong <hidellok@gmail.com>
2022-06-07 22:07:58 +0900

    annotation added in shader code

--- shader/lighting.fs
+++ shader/lighting.fs
@@ -6,9 +6,9 @@ in vec2 texCoord;
 out vec4 fragColor;
 
 uniform vec3 lightPos;
-uniform vec3 lightColor;
-uniform vec3 objectColor;
-uniform float ambientStrength;
+uniform vec3 lightColor;        // 광원의 색상
+uniform vec3 objectColor;       
+uniform float ambientStrength;  // 주변광(물체의 기본 색상)의 전체 밝기를 나타냄
 // uniform sampler2D tex;
 // uniform sampler2D tex2;
 
@@ -16,8 +16,15 @@ void main() {
     vec3 ambient = ambientStrength * lightColor;
 
     vec3 lightDir = normalize(lightPos - position);
+
+    // openGL program -> vertex shader -> 에서 넘어온 normal을 다시 normalize 하는 이유
+    // vertex shader에서 계산된 normal은 rasterization과정에서 선형보간이 진행됨
+    // 단위벡터간의 선형보간 결과가 단위벡터임을 보장하지 못하기때문에 다시 normalize과정을 fragment shader과정에 진행
     vec3 pixelNorm = normalize(normal);
+
+
     vec3 diffuse = max(dot(pixelNorm, lightDir), 0.0) * lightColor; // ignore minus val of dot product
+
 
     vec3 result = (ambient + diffuse) * objectColor;
 
--- shader/lighting.vs
+++ shader/lighting.vs
@@ -7,9 +7,11 @@ uniform mat4 transform;
 
 // local coordi 를 world coordi 로 변환하는 행렬
 // 조명은 world coordi 어딘가에서 입사됨
+// diffuse value 를 계산할려면 world space상의 좌표값이 필요
+// 법선벡터도 같은이유로 model transform만 적용 
 uniform mat4 modelTransform;
-
 out vec3 normal;
+
 out vec2 texCoord;
 out vec3 position;
 
@@ -17,7 +19,9 @@ void main() {
     
     gl_Position = transform * vec4(aPos, 1.0);
 
-    //normal = aNormal;
+    // normal = aNormal;
+    // 보통의 경우 inverse Transform model 변환을 glm등으로 계산하여 쉐이더에 적용함
+    // 아래의 코드에서 (transpose(inverse(modelTransform)) 에 해당함
     normal = (transpose(inverse(modelTransform)) * vec4(aNormal, 0.0)).xyz;
 
     texCoord    = aTexCoord;
